d737ed5b5e2943086223d92459b5ca32da64ac4f
rockllama21 <bball33336@aol.com>
2013-06-26 11:17:21 -0400

    Initial Commit

---
+++ AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Pong
+//
+//  Created by iD Student on 6/26/13.
+//  Copyright (c) 2013 Tyler Maher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pong
+//
+//  Created by iD Student on 6/26/13.
+//  Copyright (c) 2013 Tyler Maher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
